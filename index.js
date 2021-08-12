4675ec60d8a2d5b84b0a3425b01f9dd89c8bce6d
SpaceCrocodile <hicrocodile@gmail.com>
2021-08-12 15:43:57 -0400

    First Commit Ramyun

--- index.js
+++ index.js
@@ -3,28 +3,44 @@ const Page = require("./_layout/Default");
 
 module.exports = class extends Page {
     constructor(){
-        super({title:"Home", sName:"Richard Hildred"});
+        super({title:"Home", sName:"Kim"});
     }
     render(sPage) {
-        const oJson = fetch("https://prog8110summer2021-default-rtdb.firebaseio.com/meals.json").json();
-        console.log(oJson);
+        // const oJson = fetch("https://prog8110summer2021-default-rtdb.firebaseio.com/meals.json").json();
+        // console.log(oJson);
         let sResult = "<h1>Upcoming Popup Meals</h1>";
-        Object.keys(oJson).map((key) => {
-            const oEntity = oJson[key];
-            console.log(oEntity);
-            oEntity.id = key;
-            sResult += `
-            <h2>${oEntity.title}</h2>
-            <p><img src="${oEntity.featured_image}" alt="${oEntity.title}"</p>
-            <p>${oEntity.full_description}</p>
-            <form action="https://serene-taiga-04277.herokuapp.com/payment" method="post">
-            <input type="hidden" name="title" value="${oEntity.title}" />
-            <input type="hidden" name="price" value="21" />
-            <input type="tel" placeholder="enter your number" name="telephone"/>
-            <button type="submit">Order now</button>
+        // Object.keys(oJson).map((key) => {
+        //     const oEntity = oJson[key];
+        //     console.log(oEntity);
+        //     oEntity.id = key;
+            sResult += `<form name="orderForm">
+            <h2>Korean Ramyun</h2>
+            <p><img src="img/ramyun.png" alt="Summer supper"</p>
+            <!-- <form action="https://serene-taiga-04277.herokuapp.com/payment" method="post"> -->
+            <input type="hidden" name="title" value="Korean Ramen" />
+            <input type="hidden" name="price" id="priceForm" value="NA" />
+            <p><b>Choose your meat</b></p>
+            <label><input type="radio" name="meat" value="vegetarian"> Vegetarian</label>&nbsp;
+            <label><input type="radio" name="meat" value="beef"> Beef</label>&nbsp;
+            <label><input type="radio" name="meat" value="pork"> Pork</label>&nbsp;
+            <label><input type="radio" name="meat" value="chicken"> Chicken</label>&nbsp;<br><br>
+            <p><b>Choose spiciness level</b></p>
+            <label><input type="radio" name="spicy" value="1"> 1</label>&nbsp;
+            <label><input type="radio" name="spicy" value="2"> 2</label>&nbsp;
+            <label><input type="radio" name="spicy" value="3"> 3</label>&nbsp;
+            <label><input type="radio" name="spicy" value="4"> 4</label>&nbsp;
+            <label><input type="radio" name="spicy" value="5"> 5</label>&nbsp;<br><br>
+            <p><b>Side</b></p>
+            <label><input type="checkbox" name="side" value="kimchi"> Kimchi</label>&nbsp;
+            <label><input type="checkbox" name="side" value="sRadish"> Sweet Radish</label>&nbsp;<br><br>
+            <p><b>Drink</b></p>
+            <label><input type="checkbox" name="drink" value="coke"> Coke</label>&nbsp;<br><br>
+
+            <input type="tel" id="phoneInput" placeholder="enter your number" name="telephone"/>
             </form>
+            <button id="orderBtn">Order now</button>
             `;
-        });
+        // });
         return sResult;
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,1 +1,82 @@
 // your javascript here
+const DRINK_PRICE = 3;
+
+let meatPriceObj = {
+    "chicken": 15,
+    "pork": 15,
+    "beef": 17,
+    "vegetarian": 13
+  }
+  
+  let sidePriceObj = {
+    "kimchi": 4,
+    "radish": 3
+  }
+  
+  let inputValidationObj = {
+    "meat": ["chicken", "pork", "beef", "vegetarian"],
+    "kimchi": ["yes", "no"],
+    "spicy": ['0', '1', '2', '3', '4', '5'], 
+    "radish": ["yes", "no"]
+  }
+  
+  function getPrice(meat, kimchi, radish, drink) {
+    let meatPrice = meatPriceObj[meat.toLowerCase()];
+    let sidePrice = 0;
+    let drinkPrice = 0;
+    if (kimchi) {
+      sidePrice += sidePriceObj[kimchi.toLowerCase()];
+    }
+    if (radish) {
+      sidePrice += sidePriceObj[radish.toLowerCase()];
+    }
+    if (drink) {
+      drinkPrice = DRINK_PRICE;
+    }
+    let totalPrice = (meatPrice + sidePrice + drinkPrice) + (meatPrice + sidePrice + drinkPrice) * 0.07;
+    return totalPrice;
+  }
+
+  $("#orderBtn").click(function() {
+      let meat = $("input[name='meat']:checked").val();
+      let spicy  = $("input[name='spicy']:checked").val();
+      let kimchi = undefined;
+      let sRadish = undefined;
+      let drink = $("input[name='drink']:checked").val();
+
+      if (meat == undefined || spicy == undefined) {
+        alert("You must select Meat and Spiciness level");
+        return;
+      }
+      $("input[name='side']:checked").each(function(i, v) {
+        if (i == 0) {
+          kimchi = "kimchi";
+        } else {
+          sRadish = "radish";
+        }
+      });
+      console.log(getPrice(meat, kimchi, sRadish, drink));
+      $("#priceForm").val(getPrice(meat, kimchi, sRadish, drink));
+      /*<form action="https://serene-taiga-04277.herokuapp.com/payment" method="post"> -->
+            <input type="hidden" name="title" value="Summer supper" />
+            <input type="hidden" name="price" value="21" />*/
+      var queryString = $("form[name=orderForm]").serialize();
+      console.log(queryString);
+      $.ajax({
+        type : 'post',
+        crossDomain: true,
+        //headers: "Access-Control-Allow-Origin: *",
+        url : 'https://mobile-ramen-order.herokuapp.com/payment',
+        data : queryString,
+        dataType : 'jsonp',
+        error: function(xhr, status, error){
+          console.log(status);
+            alert(error);
+        },
+        success : function(json){
+          console.log("sus");
+          console.log(json);
+            alert(json)
+        }
+    });
+  });
